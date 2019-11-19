7e77cc28e67624a0b28d04b01af76eda94892dae
Niyati Bafna <niyatibafna13@gmail.com>
2019-11-19 16:59:39 +0530

    Displaying top n words of each document to understand clustering, also cosine similarities.

--- clustering_tfidf.py
+++ clustering_tfidf.py
@@ -1,5 +1,7 @@
 import numpy as np
+import pandas as pd
 from sklearn.cluster import KMeans
+import sklearn.metrics.pairwise as sk
 from sklearn.feature_extraction.text import TfidfVectorizer 
 # number_of_files_test = 1
 # files_test = [""]*number_of_files_test
@@ -12,16 +14,12 @@ files = [""]*number_of_files
 for i in range(number_of_files):
 	files[i] = open(str(i+1)+".", "r")
 	files[i].seek(0)
-# docs = [files[i].read() for i in range(number_of_files)]
 tfidf_vectorizer=TfidfVectorizer(input = 'file', use_idf=True)
 tfidf_vectorizer_vectors=tfidf_vectorizer.fit_transform(files)
-print(tfidf_vectorizer_vectors.shape)
-print(tfidf_vectorizer_vectors)
-print(type(tfidf_vectorizer_vectors))
+# print(tfidf_vectorizer_vectors.shape)
+# print(type(tfidf_vectorizer_vectors))
 tfidf_vectorizer_vectors.toarray()
-print(type(tfidf_vectorizer_vectors))
-print(tfidf_vectorizer.get_feature_names())
-# print(tfidf_vectorizer_vectors[0][7])
+# print(type(tfidf_vectorizer_vectors))
 Kmean = KMeans(n_clusters=2)
 labels = Kmean.fit_predict(tfidf_vectorizer_vectors)
 centroids = Kmean.cluster_centers_
@@ -29,6 +27,17 @@ print(centroids)
 print(labels)
 print(Kmean.predict(tfidf_vectorizer_vectors))
 
+features = tfidf_vectorizer.get_feature_names()
+tfidf_array_format = tfidf_vectorizer_vectors.toarray()
 for i in range(number_of_files):
-	print((tfidf_vectorizer_vectors[i][2]))
-
+	row = tfidf_array_format[i]
+	topn_ids = np.argsort(row)[::-1][:20]
+	# print(topn_ids)
+	# print(row[topn_ids[0]])
+	top_feats = [(features[j], row[j]) for j in topn_ids]
+	df = pd.DataFrame(top_feats, columns=['features', 'score'])
+	print("Top 20 words and scores in document: ")
+	print(df)
+cosine_similarities = sk.cosine_similarity(tfidf_array_format)
+print("Printing cosine cosine_similarities pairwise:")
+print(cosine_similarities)
