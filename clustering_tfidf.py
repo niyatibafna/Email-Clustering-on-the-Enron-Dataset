edf01cee81b88f234f7f503636d613d2932b8820
Niyati Bafna <niyatibafna13@gmail.com>
2019-11-19 13:22:00 +0530

    Labelling in order to examine individual clusters

--- clustering_tfidf.py
+++ clustering_tfidf.py
@@ -1,10 +1,17 @@
 import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer 
+# number_of_files_test = 1
+# files_test = [""]*number_of_files_test
+# for i in range(number_of_files_test):
+# 	files_test[i] = open(str(i+1)+".", "r")
+# tfidf_vectorizer_test = TfidfVectorizer(input = 'file', use_idf=True)
+# tfidf_vectorizer_test_vectors=tfidf_vectorizer_test.fit_transform(files_test)
 number_of_files = 5
 files = [""]*number_of_files
 for i in range(number_of_files):
 	files[i] = open(str(i+1)+".", "r")
+	files[i].seek(0)
 # docs = [files[i].read() for i in range(number_of_files)]
 tfidf_vectorizer=TfidfVectorizer(input = 'file', use_idf=True)
 tfidf_vectorizer_vectors=tfidf_vectorizer.fit_transform(files)
@@ -16,6 +23,9 @@ print(type(tfidf_vectorizer_vectors))
 print(tfidf_vectorizer.get_feature_names())
 # print(tfidf_vectorizer_vectors[0][7])
 Kmean = KMeans(n_clusters=2)
-Kmean.fit(tfidf_vectorizer_vectors)
+labels = Kmean.fit_predict(tfidf_vectorizer_vectors)
 centroids = Kmean.cluster_centers_
 print(centroids)
+print(labels)
+print(Kmean.predict(tfidf_vectorizer_vectors))
+
