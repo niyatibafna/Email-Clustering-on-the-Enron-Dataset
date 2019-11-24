f55dea49f90d83f25a9e2b9c6be49f83e0dc30c2
Niyati Bafna <niyatibafna13@gmail.com>
2019-11-24 17:21:06 +0530

    Running Kmeans over all cleaned data, saving model, finding intertia

--- clustering_tfidf.py
+++ clustering_tfidf.py
@@ -4,6 +4,7 @@ from sklearn.cluster import KMeans
 import sklearn.metrics.pairwise as sk
 from sklearn.feature_extraction.text import TfidfVectorizer 
 import nltk
+from joblib import dump
 
 NUMBER_OF_FILES = 517402
 
@@ -16,38 +17,45 @@ NUMBER_OF_FILES = 517402
 files = [""]*NUMBER_OF_FILES
 content = [""]*NUMBER_OF_FILES
 for i in range(NUMBER_OF_FILES):
-	files[i] = open(str(i+1)+".", "r")
-	text = nltk.word_tokenize(files[i].read())
-	text = nltk.pos_tag(text)
-	for j in range(len(text)):
-		if(text[j][1].startswith("N") or text[j][1].startswith("V")):
-			content[i] = content[i] + " "+ text[j][0]
-	files[i].seek(0)
+	files[i] = open("clean/"+str(i+100000)+".txt", "r")
+	content[i] = files[i].read()
+	files[i].close()
+	# text = nltk.pos_tag(text)
+	# for j in range(len(text)):
+	# 	if(text[j][1].startswith("N") or text[j][1].startswith("V")): #Clustering only on nouns and verbs
+	# 		content[i] = content[i] + " "+ text[j][0]
 tfidf_vectorizer=TfidfVectorizer(input = 'content', use_idf=True)
 tfidf_vectorizer_vectors=tfidf_vectorizer.fit_transform(content)
-# print(tfidf_vectorizer_vectors.shape)
-# print(type(tfidf_vectorizer_vectors))
 tfidf_vectorizer_vectors.toarray()
 # print(type(tfidf_vectorizer_vectors))
-Kmean = KMeans(n_clusters=2)
-labels = Kmean.fit_predict(tfidf_vectorizer_vectors)
-centroids = Kmean.cluster_centers_
-print(centroids)
-print(labels)
-print(Kmean.predict(tfidf_vectorizer_vectors))
+good_topics = [11]
+for i in good_topics:
+	Kmean = KMeans(n_clusters=i, n_jobs = 10, n_init = 10)
+	labels = Kmean.fit_predict(tfidf_vectorizer_vectors)
+	centroids = Kmean.cluster_centers_
+	print(Kmean.inertia_)
+	model_name = 'Kmeans'+str(i) + '.model'
+	#SAVE MODEL
+	dump(Kmean, model_name)
 
-features = tfidf_vectorizer.get_feature_names()
-print(features[i])
-tfidf_array_format = tfidf_vectorizer_vectors.toarray()
-for i in range(NUMBER_OF_FILES):
-	row = tfidf_array_format[i]
-	topn_ids = np.argsort(row)[::-1][:20]
-	# print(topn_ids)
-	# print(row[topn_ids[0]])
-	top_feats = [(features[j], row[j]) for j in topn_ids]
-	df = pd.DataFrame(top_feats, columns=['features', 'score'])
-	print("Top 20 words and scores in document: ")
-	print(df)
-cosine_similarities = sk.cosine_similarity(tfidf_array_format)
-print("Printing cosine cosine_similarities pairwise:")
-print(cosine_similarities)
+
+
+
+# features = tfidf_vectorizer.get_feature_names()
+# print(features[i])
+# tfidf_array_format = tfidf_vectorizer_vectors.toarray()
+
+
+#For understanding output
+# for i in range(NUMBER_OF_FILES):
+# 	row = tfidf_array_format[i]
+# 	topn_ids = np.argsort(row)[::-1][:20]
+# 	# print(topn_ids)
+# 	# print(row[topn_ids[0]])
+# 	top_feats = [(features[j], row[j]) for j in topn_ids]
+# 	df = pd.DataFrame(top_feats, columns=['features', 'score'])
+# 	print("Top 20 words and scores in document: ")
+# 	print(df)
+# # cosine_similarities = sk.cosine_similarity(tfidf_array_format)
+# # print("Printing cosine cosine_similarities pairwise:")
+# # print(cosine_similarities)
