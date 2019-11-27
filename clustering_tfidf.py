d4156689530cb1bb1499a7de85db4d45909f5cc4
Niyati Bafna <niyatibafna13@gmail.com>
2019-11-27 20:23:32 +0530

    Calculating Silhouette Score of clustered data in order to understand whether clustering is a good idea. Using 'good' cluster numbers obtained from other analysis. Also calculating inertia.

--- clustering_tfidf.py
+++ clustering_tfidf.py
@@ -4,9 +4,11 @@ from sklearn.cluster import KMeans
 import sklearn.metrics.pairwise as sk
 from sklearn.feature_extraction.text import TfidfVectorizer 
 import nltk
+from sklearn import metrics
+from sklearn.metrics import pairwise_distances
 from joblib import dump
 
-NUMBER_OF_FILES = 517402
+NUMBER_OF_FILES = 517401
 
 # NUMBER_OF_FILES_test = 1
 # files_test = [""]*NUMBER_OF_FILES_test
@@ -16,8 +18,8 @@ NUMBER_OF_FILES = 517402
 # tfidf_vectorizer_test_vectors=tfidf_vectorizer_test.fit_transform(files_test)
 files = [""]*NUMBER_OF_FILES
 content = [""]*NUMBER_OF_FILES
-for i in range(NUMBER_OF_FILES):
-	files[i] = open("clean/"+str(i+100000)+".txt", "r")
+for i in range(1, NUMBER_OF_FILES):
+	files[i] = open("clean/"+str(i)+".txt", "r")
 	content[i] = files[i].read()
 	files[i].close()
 	# text = nltk.pos_tag(text)
@@ -28,12 +30,15 @@ tfidf_vectorizer=TfidfVectorizer(input = 'content', use_idf=True)
 tfidf_vectorizer_vectors=tfidf_vectorizer.fit_transform(content)
 tfidf_vectorizer_vectors.toarray()
 # print(type(tfidf_vectorizer_vectors))
-good_topics = [11]
+good_topics = [7,11]
 for i in good_topics:
 	Kmean = KMeans(n_clusters=i, n_jobs = 10, n_init = 10)
-	labels = Kmean.fit_predict(tfidf_vectorizer_vectors)
-	centroids = Kmean.cluster_centers_
-	print(Kmean.inertia_)
+	predicted = Kmean.fit_predict(tfidf_vectorizer_vectors)
+	labels = Kmean.labels_
+	silhouette = metrics.silhouette_score(tfidf_vectorizer_vectors, labels, metric='euclidean')
+	print("Silhouette score for model", i, " ", silhouette)
+	# centroids = Kmean.cluster_centers_
+	print("Inertia: ", Kmean.inertia_)
 	model_name = 'Kmeans'+str(i) + '.model'
 	#SAVE MODEL
 	dump(Kmean, model_name)
